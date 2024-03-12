ba9920e98c07518966a1167ea285d0944671c7be
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-03-12 14:04:03 +0545

    CRUD

--- Course/course.routes.js
+++ Course/course.routes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Course from "./course.model.js";
-import { courseValidationSchema } from "./course.validation.js";
+import { courseValidationSchema, paginationDataValidationSchema } from "./course.validation.js";
+import mongoose from "mongoose";
 
 const router = express.Router();
 
@@ -16,6 +17,81 @@ router.post("/course/add", async (req, res) => {
   await Course.create(newCourse);
 
   return res.status(201).send({ message: "Course added successfully." });
+});
+
+router.put("/course/edit/:id", async (req, res) => {
+  const courseId = req.params.id;
+  const isValidMongoId = mongoose.isValidObjectId(courseId);
+
+  if (!isValidMongoId) {
+    return res.status(404).send({ message: "Invalid mongo id" });
+  }
+
+  const requiredCourse = await Course.findOne({ _id: courseId });
+
+  if (!requiredCourse) {
+    return res.status(404).send({ message: "Course does not exist" });
+  }
+
+  const newValues = req.body;
+
+  try {
+    await courseValidationSchema.validate(newValues);
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
+  }
+
+  await Course.updateOne(
+    { _id: courseId },
+    {
+      $set: {
+        ...newValues,
+      },
+    }
+  );
+
+  return res.status(200).send({ message: "Course updated successfully" });
+});
+
+router.delete("/course/delete/:id", async (req, res) => {
+  const courseId = req.params.id;
+  const isValidMongoId = mongoose.isValidObjectId(courseId);
+
+  if (!isValidMongoId) {
+    return res.status(404).send({ message: "Invalid mongo id" });
+  }
+
+  const requiredCourse = await Course.findOne({ _id: courseId });
+
+  if (!requiredCourse) {
+    return res.status(404).send({ message: "Course does not exist" });
+  }
+
+  await Course.deleteOne({ _id: courseId });
+
+  return res.status(200).send({ message: "Course deleted successfully" });
+});
+
+router.get("/course/list", async (req, res) => {
+  const paginationData = req.body;
+  let validatedData;
+
+  try {
+    validatedData = await paginationDataValidationSchema.validate(paginationData);
+  } catch (error) {
+    return res.status(400).send({ message: error.message });
+  }
+
+  const skip = (validatedData.page - 1) * validatedData.limit;
+
+  const courses = await Course.aggregate([
+    { $match: {} },
+    { $skip: skip },
+    { $limit: validatedData.limit },
+    { $project: { name: 1, duration: 1, price: 1, tutorName: 1 } },
+  ]);
+
+  return res.status(200).send({ message: "Success", courseList: courses });
 });
 
 export default router;
--- Course/course.validation.js
+++ Course/course.validation.js
@@ -3,6 +3,11 @@ import Yup from "yup";
 export const courseValidationSchema = Yup.object({
   name: Yup.string().trim().required().max(45, "Name must be at max 45 characters."),
   duration: Yup.number().min(1, "Duration must be at least 1 day.").required(),
-  price: Yup.number().min(0, "Price must be at least positive").required(),
+  price: Yup.number().min(0, "Price must be at least 0.").required(),
   tutorName: Yup.string().trim().max(45, "Tutor name must be at max 45 characters."),
+});
+
+export const paginationDataValidationSchema = Yup.object({
+  page: Yup.number().required().positive().min(1, "Page must be at least 1."),
+  limit: Yup.number().default(6).min(1, "Limit must be at least 1."),
 });
